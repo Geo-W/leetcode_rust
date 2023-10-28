7f222dd136a3e69ed2e95d710fce6f774080ae04
Geo-W <geo.wgy@gmail.com>
2023-10-29 00:25:54 +0800

    _0274_h_index.rs

---
+++ src/standard/_0274_h_index.rs
@@ -0,0 +1,38 @@
+/// Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+/// The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+pub fn h_index(citations: Vec<i32>) -> i32 {
+    let mut citations = citations;
+    citations.sort_by(|x, y| y.cmp(x));
+    let mut ret = 0;
+    for (i, x) in citations.iter().enumerate() {
+        if *x >= (i + 1) as i32 {
+            ret +=1;
+        }
+    }
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a() {
+        assert_eq!(h_index(vec![3, 0, 6, 1, 5]), 3);
+    }
+
+    #[test]
+    fn b() {
+        assert_eq!(h_index(vec![1, 3, 1]), 1);
+    }
+
+    #[test]
+    fn c() {
+        assert_eq!(h_index(vec![11, 15]), 2);
+    }
+    
+    #[test]
+    fn d() {
+        assert_eq!(h_index(vec![100]), 1);
+    }
+}
