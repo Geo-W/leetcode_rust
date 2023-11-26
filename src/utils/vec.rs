8b57a95e86a94a373c583f8c0431cb9347ceb4c0
geo <geo.wgy@gmail.com>
2023-11-26 23:25:51 +0800

    add vec_vec macro

--- src/utils/vec.rs
+++ src/utils/vec.rs
@@ -9,3 +9,15 @@ macro_rules! vec_string {
         temp_vec
     }};
 }
+
+#[macro_export]
+macro_rules! vec_vec {
+    () => {
+        Vec::new()
+    };
+        ($($element:expr),* $(,)?) => {{
+        let mut temp_vec = Vec::new();
+        $(temp_vec.push($element.to_vec());)*
+        temp_vec
+    }};
+}
