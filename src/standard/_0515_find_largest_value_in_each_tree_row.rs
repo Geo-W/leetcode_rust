e4d6e1fc9e0f13e63d575d7ee8586a53e07b3f93
Geo-W <geo.wgy@gmail.com>
2024-07-17 16:09:32 +0800

    _0515_find_largest_value_in_each_tree_row.rs

---
+++ src/standard/_0515_find_largest_value_in_each_tree_row.rs
@@ -0,0 +1,41 @@
+use crate::utils::tree::TreeNode;
+use std::cell::RefCell;
+use std::cmp::max;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+pub fn largest_values(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut deque = VecDeque::new();
+    let mut ret = vec![];
+    deque.push_back(root.unwrap());
+    while !deque.is_empty() {
+        let mut tmp = 0;
+        for _ in 0..deque.len() {
+            let rc = deque.pop_front().unwrap();
+            let node = rc.borrow();
+            if let Some(v) = &node.left {
+                deque.push_back(v.clone());
+            }
+            if let Some(v) = &node.right {
+                deque.push_back(v.clone());
+            }
+            tmp = max(tmp, node.val);
+        }
+        ret.push(tmp);
+    }
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tree_from_vec;
+
+    #[test]
+    fn a() {
+        assert_eq!(
+            largest_values(tree_from_vec![1, 3, 2, 5, 3, null, 9]),
+            vec![1, 3, 9]
+        );
+    }
+}
